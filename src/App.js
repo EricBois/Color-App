b7d45a9581f0b77853a61c23ad1d7818bc775ee1
Eric Boisvenu <burn4live@gmail.com>
2020-10-29 10:25:49 -0600

    added colorHelpers with chroma

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import Palette from './Palette';
 import seedColors from './seedColors';
+import { generatePalette } from './colorHelpers';
 
 class App extends Component {
+
   render() {
     return (
       <div>
