d01d4606a8bd151eb60a90c183b2332869436d97
Eric Boisvenu <burn4live@gmail.com>
2020-10-31 10:49:23 -0600

    added link to palette

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -34,6 +34,9 @@ const styles = {
 };
 
 class PaletteList extends Component {
+  gotToPalette(id) {
+    this.props.history.push(`/palette/${id}`)
+  }
   render() {
     const { palettes, classes } = this.props;
     return (
@@ -44,7 +47,7 @@ class PaletteList extends Component {
           </nav>
           <div className={classes.palettes}>
             {palettes.map((palette) => (
-              <MiniPalette {...palette} />
+              <MiniPalette {...palette} handleClick={() =>this.gotToPalette(palette.id)} />
             ))}
           </div>
         </div>
