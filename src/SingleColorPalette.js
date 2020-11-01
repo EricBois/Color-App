5dee92eb259834521eaa720f473523b8fdebc24c
Eric Boisvenu <burn4live@gmail.com>
2020-11-01 09:19:39 -0700

    add Go back link

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
 import Navbar from './Navbar';
-import PaletteFooter from './PaletteFooter'; 
+import { Link } from 'react-router-dom';
+import PaletteFooter from './PaletteFooter';
 
 class SingleColorPalette extends Component {
   constructor(props) {
     super(props);
     this._shades = this.gatherShades(this.props.palette, this.props.colorId);
-    this.state = { format: 'hex'};
+    this.state = { format: 'hex' };
     this.changeFormat = this.changeFormat.bind(this);
   }
   gatherShades(palette, colorToFilterBy) {
@@ -25,16 +26,26 @@ class SingleColorPalette extends Component {
     this.setState({ format: val });
   }
   render() {
-    const { paletteName, emoji } =  this.props.palette;
+    const { paletteName, emoji, id } = this.props.palette;
     const { format } = this.state;
-    const colorBoxes = this._shades.map(color => (
-      <ColorBox key={color.hex} name={color.name} background={color[format]} showLink={false} />
-    ))
+    const colorBoxes = this._shades.map((color) => (
+      <ColorBox
+        key={color.hex}
+        name={color.name}
+        background={color[format]}
+        showLink={false}
+      />
+    ));
     return (
-      <div className="Palette">
+      <div className="SingleColorPalette Palette">
         <Navbar handleChange={this.changeFormat} showingAllColors={false} />
         <h1>Single color palette</h1>
-        <div className="Palette-colors">{colorBoxes}</div>
+        <div className="Palette-colors">
+          {colorBoxes}
+          <div className="go-back ColorBox">
+            <Link to={`/palette/${id}`} className="back-button">Go Back</Link>
+          </div>
+        </div>
         <PaletteFooter emoji={emoji} paletteName={paletteName} />
       </div>
     );
