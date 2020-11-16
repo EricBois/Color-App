5235fa2867468880e334b60bb87627ee77e30318
Eric Boisvenu <burn4live@gmail.com>
2020-11-16 15:09:50 -0700

    add bg svg to paletteList

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,12 +1,19 @@
 import sizes from './sizes';
+import bg from './bg.svg';
 
 const styles = {
   root: {
-    backgroundColor: 'blue',
     height: '100vh',
     display: 'flex',
     alignItems: 'flex-start',
     justifyContent: 'center',
+    /* background by SVGBackgrounds.com */
+    backgroundColor: '#2023aa',
+    backgroundImage: `url(${bg})`,
+    overflow: 'scroll',
+  },
+  heading: {
+    fontSize: '2rem',
   },
   container: {
     width: '50%',
